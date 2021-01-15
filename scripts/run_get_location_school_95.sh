d74bef7aa146b7745d70d080054e80b7a0aa4dbe
Vlad Lialin <guitaricet@gmail.com>
2021-01-14 20:20:31 -0500

    update school script

--- scripts/run_get_location_school_95.sh
+++ scripts/run_get_location_school_95.sh
@@ -3,23 +3,27 @@ cd ..
 
 
 SET_NAME=school_95
-DATE=Oct27
+DATE=Jan14
 CLASSES=IN:GET_LOCATION_SCHOOL
 
 DATA=data-bin/"$SET_NAME"_"$DATE"
-MODEL=output_dir/"$SET_NAME"_"$DATE"_bert_run
-BATCH_SIZE=96
-ACCUM_STEPS=2
+MODEL=output_dir/"$SET_NAME"_"$DATE"
+FINETUNED="$MODEL"_finetuned
+BATCH_SIZE=112
 SPLIT=0.95
+
+export TOKENIZERS_PARALLELISM=false
 
 
 python cli/preprocess.py \
-  --data data/top-dataset-semantic-parsing \
+  --data data/snips/top_format \
   --text-tokenizer bert-base-cased \
   --output-dir $DATA \
   --split-class $CLASSES \
   --split-amount $SPLIT \
 
+
+TAG="$SET_NAME"_"$DATE"
 
 python cli/train.py \
   --data-dir $DATA  \
@@ -27,160 +31,154 @@ python cli/train.py \
   --decoder-lr 0.2 \
   --encoder-lr 0.02 \
   --batch-size $BATCH_SIZE \
-  --gradient-accumulation-steps 2 \
   --layers 4 \
   --hidden 256 \
   --dropout 0.2 \
   --heads 4 \
-  --label-smoothing 0.1 \
+  --label-smoothing 0.0 \
   --epochs 100 \
+  --early-stopping 10 \
   --warmup-steps 1500 \
   --freeze-encoder 0 \
   --unfreeze-encoder 500 \
   --log-every 150 \
-  --early-stopping 10 \
   --output-dir $MODEL \
   --tags train,$TAG \
   --new-classes $CLASSES \
+  --track-grad-square \
   --seed 1 \
 
 
-# baseline
-TAG="$SET_NAME"_"$DATE"_bert_run_baseline
-# path_99_Aug19_bert_run_baseline
+TAG="$SET_NAME"_"$DATE"_replay
 
-
-for old_data_amount in 0.0 0.01 0.05 0.1 0.15 0.2 0.3 0.5 0.7 1.0
+for old_data_amount in 0.0 0.05 0.1 0.2 0.5 0.7 1.0
 do
 
-    rm -rf output_dir/finetuned
+    rm -rf $FINETUNED
 
     python cli/retrain.py \
       --data-dir $DATA \
       --model-dir $MODEL \
       --batch-size $BATCH_SIZE \
-      --gradient-accumulation-steps 2 \
       --dropout 0.2 \
-      --epochs 40 \
-      --early-stopping 20 \
+      --epochs 50 \
+      --early-stopping 10 \
       --log-every 100 \
       --new-data-amount 1.0 \
       --old-data-amount $old_data_amount \
       --new-classes $CLASSES \
       --tags finetune,$TAG \
-      --output-dir output_dir/finetuned \
-
+      --output-dir $FINETUNED \
+      --old-data-sampling-method merge_subset \
 
 done
 
+TAG="$SET_NAME"_"$DATE"_sample
 
-# sample
-TAG="$SET_NAME"_"$DATE"_bert_run_sample
-
-
-for old_data_amount in 0.0 0.01 0.05 0.1 0.15 0.2 0.3 0.5 0.7 1.0
+for old_data_amount in 0.0 0.05 0.1 0.2 0.5 0.7 1.0
 do
 
-    rm -rf output_dir/finetuned
+    rm -rf $FINETUNED
 
     python cli/retrain.py \
       --data-dir $DATA \
       --model-dir $MODEL \
       --batch-size $BATCH_SIZE \
-      --gradient-accumulation-steps 2 \
       --dropout 0.2 \
-      --epochs 40 \
-      --early-stopping 20 \
+      --epochs 50 \
+      --early-stopping 10 \
       --log-every 100 \
       --new-data-amount 1.0 \
       --old-data-amount $old_data_amount \
+      --new-classes $CLASSES \
+      --tags finetune,$TAG \
+      --output-dir $FINETUNED \
       --old-data-sampling-method sample \
-      --new-classes $CLASSES \
-      --tags finetune,$TAG \
-      --output-dir output_dir/finetuned \
-
 
 done
 
-# move norm 0.05
-TAG="$SET_NAME"_"$DATE"_bert_run_move_norm
 
-for old_data_amount in 0.0 0.01 0.05 0.1 0.15 0.2 0.3 0.5 0.7 1.0
+TAG="$SET_NAME"_"$DATE"_ewc_replay
+
+for old_data_amount in 0.0 0.05 0.1 0.2 0.5 0.7 1.0
 do
 
-    rm -rf output_dir/finetuned
+    ewc=100
+
+    rm -rf $FINETUNED
 
     python cli/retrain.py \
       --data-dir $DATA \
       --model-dir $MODEL \
       --batch-size $BATCH_SIZE \
-      --gradient-accumulation-steps 2 \
       --dropout 0.2 \
-      --move-norm 0.1 \
-      --epochs 40 \
+      --epochs 50 \
       --early-stopping 10 \
       --log-every 100 \
       --new-data-amount 1.0 \
       --old-data-amount $old_data_amount \
+      --weight-consolidation $ewc \
       --new-classes $CLASSES \
-      --tags finetune,$TAG \
-      --output-dir output_dir/finetuned \
-
+      --tags finetune,$TAG,ewc_"$ewc" \
+      --output-dir $FINETUNED \
+      --old-data-sampling-method merge_subset \
 
 done
 
-# freeze encoder
-TAG="$SET_NAME"_"$DATE"_bert_run_freeze
 
-for old_data_amount in 0.0 0.01 0.05 0.1 0.15 0.2 0.3 0.5 0.7 1.0
+TAG="$SET_NAME"_"$DATE"_ewc_sample
+
+for old_data_amount in 0.0 0.05 0.1 0.2 0.5 0.7 1.0
 do
 
-    rm -rf output_dir/finetuned
+    ewc=100
+
+    rm -rf $FINETUNED
 
     python cli/retrain.py \
       --data-dir $DATA \
       --model-dir $MODEL \
       --batch-size $BATCH_SIZE \
-      --gradient-accumulation-steps 2 \
       --dropout 0.2 \
-      --freeze-encoder 0 \
-      --epochs 40 \
+      --epochs 50 \
       --early-stopping 10 \
       --log-every 100 \
       --new-data-amount 1.0 \
       --old-data-amount $old_data_amount \
+      --weight-consolidation $ewc \
       --new-classes $CLASSES \
-      --tags finetune,$TAG \
-      --output-dir output_dir/finetuned \
-
-
-done
-
-# best recipe
-TAG="$SET_NAME"_"$DATE"_bert_run_best
-
-for old_data_amount in 0.0 0.01 0.05 0.1 0.15 0.2 0.3 0.5 0.7 1.0
-do
-
-    rm -rf output_dir/finetuned
-
-    python cli/retrain.py \
-      --data-dir $DATA \
-      --model-dir $MODEL \
-      --batch-size $BATCH_SIZE \
-      --gradient-accumulation-steps 2 \
-      --dropout 0.2 \
-      --move-norm 0.1 \
-      --freeze-encoder 0 \
-      --epochs 40 \
-      --early-stopping 10 \
-      --log-every 100 \
-      --new-data-amount 1.0 \
-      --old-data-amount $old_data_amount \
+      --tags finetune,$TAG,ewc_"$ewc" \
+      --output-dir $FINETUNED \
       --old-data-sampling-method sample \
+
+done
+
+
+TAG="$SET_NAME"_"$DATE"_move_norm
+
+for old_data_amount in 0.0 0.05 0.1 0.2 0.5 0.7 1.0
+do
+
+for move_norm in 0.05 0.1
+do
+
+    rm -rf $FINETUNED
+
+    python cli/retrain.py \
+      --data-dir $DATA \
+      --model-dir $MODEL \
+      --batch-size $BATCH_SIZE \
+      --dropout 0.2 \
+      --epochs 50 \
+      --early-stopping 10 \
+      --log-every 100 \
+      --new-data-amount 1.0 \
+      --old-data-amount $old_data_amount \
+      --move-norm $move_norm \
       --new-classes $CLASSES \
-      --tags finetune,$TAG \
-      --output-dir output_dir/finetuned \
+      --tags finetune,$TAG,move_norm_"$move_norm" \
+      --output-dir $FINETUNED \
+      --old-data-sampling-method merge_subset \
 
-
+done
 done
